6bbf76f9df7137596ae3742e3943c8c091455e13
Ross Hazelett <rhhazelett@gmail.com>
2020-05-04 20:32:14 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+// Styles
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { dogImage: "" };
+
+  componentDidMount() {
+    this.fetchDogImage();
+  }
+
+  fetchDogImage = () => {
+    axios
+      .get("https://random.dog/woof.json")
+      .then((response) => {
+        const { url } = response.data;
+        console.log(typeof url);
+        console.log(url);
+        // skips anything with video
+        if (url.includes("mp4") || url.includes("webm")) {
+          console.log("contains video");
+          this.fetchDogImage();
+        } else {
+          this.setState({ dogImageUrl: url });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { dogImageUrl } = this.state;
+
+    return (
+      <div className="app">
+        <div className="image-container">
+          <img className="dog-image" src={dogImageUrl} alt=""></img>
+        </div>
+        <div className="button-container">
+          <button className="dog-btn" onClick={this.fetchDogImage}>
+            Show Me Doggo{" "}
+            <span aria-label="dog emoji" role="img">
+              🐶
+            </span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
